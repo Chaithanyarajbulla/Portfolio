bad4d65d715778727621ec51bf37984279fb0fa7
Chaithanya Raj Bulla <chaithanyarajbulla@gmail.com>
2025-02-24 23:30:39 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -119,7 +119,7 @@ document
     responseMessage.style.color = "blue";
 
     try {
-      const response = await fetch("/api/send-email.js", {
+      const response = await fetch("/api/send-email", {
         // Ensure API path is correct
         method: "POST",
         headers: { "Content-Type": "application/json" },
